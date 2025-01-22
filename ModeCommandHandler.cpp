390576db92b2877e3a21b364e35fe8b8592ed852
Jeff Lim <jeffopenpacks@Jeffs-MacBook-Air.local>
2025-01-22 10:15:41 +0800

    mode parsing done

--- ModeCommandHandler.cpp
+++ ModeCommandHandler.cpp
@@ -9,8 +9,8 @@ ModeCommandHandler::ModeCommandHandler(Server& server) : CommandHandler(server) 
 static bool isChannel(const string& channelName) {
 		return !channelName.empty() && channelName[0] == '#';
 }
-//Mode doesn't change properly i think.
-static void parseModeCmd(const string& cmd, Channel* channel, Client& client, Server* server, vector<string>&modeArgs)
+//Skip all channels, then handle all modes, then match args
+static void parseModeCmd(Message* message, Channel* channel, Client& client, Server* server, size_t argPos, vector<string>&modeArgs)
 {
 	int modeflag = 1;
 	for (size_t i = 0; i < cmd.length(); ++i) {
@@ -88,42 +88,77 @@ static void parseModeCmd(const string& cmd, Channel* channel, Client& client, Se
 	}
 }
 
-void ModeCommandHandler::handle(Client& client, const Message& message) {
-        // Basic MODE implementation - just acknowledge the mode request
+static size_t findMode(const Message& message) {
+	//find mode operations immediately after channels, throw error if none
+	for (size_t i = 1; i < message.getParams().size(); ++i) {
+		if (message.getParams()[i][0] == '#') continue;
+		if (message.getParams()[i][0] == '+' || message.getParams()[i][0] == '-') {
+			return i;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
+}
 
+void ModeCommandHandler::handle(Client& client, const Message& message) {
+        // Caters to irssi mode command at connect, we don't process if it's mode user
+		if (findClientByNickname(message.getParams()[0]) != NULL) {return;}
         if (message.getParams().size() < 2 || !isChannel(message.getParams()[0])) {
                 client.send("461 " + client.getNickname() + " MODE :Not enough parameters\r\n");
                 return;
         }
-		//there must be a channel passed from client to server, else it means the command isn't entered correctly.
-        // For now, just send a generic response for user modes
-        /*if (message.getParams()[0] == "*" || message.getParams()[0] == client.getNickname()) {
-                client.send(":" + server.getServerName() + " 221 " + client.getNickname() + " +i\r\n");
-        }*/
+		//Things to check for: 
+		//2. find mode operations, throw error if none
+		//3. read until space, continue
+		//4. once space followed by non +/-, the remaining are arguments
+		//5. ignore if not valid options
+		//6. use args sequentially based on modes that need them, else throw error but continue
+		//Checks if server exists
         Channel *channel = server.getChannel(message.getParams()[0]);
         if (!channel) {
                 client.send("403 " + client.getNickname() + " " + message.getParams()[0] + " : Channel doesn't exist\r\n");
                 return;
         }
+		//check if client in channel, auth, operator
         if (!channel->hasClient(&client)) {
                 client.send("442 " + client.getNickname() + " " + message.getParams()[0] + " : You're not on that channel\r\n");
                 return;
         }
+		if (!client.isAuthenticated()) {
+			client.send("481 " + client.getNickname() + " " + message.getParams()[0] + " : You're not authenticated\r\n");
+			return;
+		}
         if (!channel->isOperator(&client)) {
                 client.send("482 " + client.getNickname() + " " + message.getParams()[0] + " : You're not a channel operator\r\n");
                 return;
         }
+		size_t argPos = findMode(message);
+		if (!argPos) {
+			//return error if no mode operations found
+			client.send("461 " + client.getNickname() + " " + message.getParams()[0] + " : No mode operations found\r\n");
+			return;
+		}
+		//skip past mode operations
+		while (argPos < message.getParams().size() && message.getParams()[argPos][0] == '+' || message.getParams()[argPos][0] == '-') {
+			argPos++;
+		}
+		//store arguments, any options encountered from here on will be treated as arg
         vector<string> modeArgs;
         if (message.getParams().size() > 2)
         {
-                for (size_t i = 2; i < message.getParams().size(); i++) {
-			cout << "args are: " << message.getParams()[i] << endl;
-                        modeArgs.push_back(message.getParams()[i]);
+                for (size_t i = argPos; i < message.getParams().size(); ++i) {
+					if (message.getParams()[i][0] == '#' || message.getParams()[i][0] == '+' || message.getParams()[i][0] == '-') {
+						continue ;
+					}
+					//s
+					cout << "args are: " << message.getParams()[i] << endl;
+                    modeArgs.push_back(message.getParams()[i]);
                 }
         }
-        string cmd = message.getParams()[1];
-        //parse through string, change flag whenever encounter +- else handle mode, take note of position of args in case mode needs args
-        parseModeCmd(cmd, channel, client, &server, modeArgs);
+
+        
+        parseModeCmd(message, channel, client, &server, argPos, modeArgs);
 
 
         //set/remove invite only
